09cf0489ba18580fa48ca8b77e8b899d690fe270
Eric Greene <eric@t4d.io>
2019-10-07 13:01:57 -0700

    add code demos

--- demo-app/src/components/ColorTool.js
+++ demo-app/src/components/ColorTool.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 export const ColorTool = () => {
 
+  // array of objects
+  const colors = [
+    { id: 1, name: 'orange' },
+    { id: 2, name: 'green' },
+    { id: 3, name: 'hot pink' },
+    { id: 4, name: 'red' },
+  ];
+
   return <>
     <header>
       <h1>Color Tool</h1>
     </header>
     <ul>
-      <li>orange</li>
-      <li>green</li>
-      <li>hot pink</li>
-      <li>red</li>
+      {colors.map( (color) =>
+        <li key={color.id}>{color.name}</li>)}
     </ul>
   </>;
 
